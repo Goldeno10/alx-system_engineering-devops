9b756a135db35bddc2bc9b9181072af807583865
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-08-21 19:30:51 +0100

    Added task 102

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int infinite_while(void);
+
+/**
+ *infinite_while - Creates an infinit loop
+ *Return: 0 (success)
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ *main - Entry point
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	pid_t ZOMBIE_PID;
+	int i = 0;
+
+	while (i < 5)
+	{
+		if ((fork()) == 0)
+		{
+			printf("Zombie process created, PID: %u\n", getpid());
+			return (0);
+		}
+		i++;
+	}
+	infinite_while();
+	return (0);
+}
