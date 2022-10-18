e82df51ff002d55b4b2799d2cee77eb6d8315cb5
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-10-18 13:54:12 +0100

    debugging pupped script

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,10 @@
 # Installing  Nginx web server
 exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html && sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default && sudo service nginx start',
+  command  => 'sudo apt-get -y update &&\
+  sudo apt-get -y install nginx &&\
+  echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html &&\
+  echo "Ceci n'est pas une page." | sudo tee /usr/share/nginx/html/custom_404.html &&\
+  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;\n\n\terror_page 404 \/custom_404.html;\n\tlocation = \/custom_404.html {\n\t\troot \/usr\/share\/nginx\/html;\n\t\tinternal;\n\t}/" /etc/nginx/sites-available/default &&\
+  sudo service nginx reload,
 }
