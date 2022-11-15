4bcb993872a66c7ea411aab5baa957abc2e08924
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-11-15 14:22:16 +0000

    fixed bug in project

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,27 +6,28 @@ import sys
 from urllib.request import urlopen
 import json
 
-user_id = int(sys.argv[1])
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
 
-todo_url = "https://jsonplaceholder.typicode.com/todos"
-with urlopen(todo_url) as todo_resp:
-    todo = todo_resp.read()
-todo_data = json.loads(todo)
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    with urlopen(todo_url) as todo_resp:
+        todo = todo_resp.read()
+    todo_data = json.loads(todo)
 
-user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-with urlopen(user_url) as user_resp:
-    user = user_resp.read()
-user_data = json.loads(user)
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    with urlopen(user_url) as user_resp:
+        user = user_resp.read()
+    user_data = json.loads(user)
 
-user_todo = [item for item in todo_data if item["userId"] == user_id]
-comp_todo = [item for item in user_todo if item["completed"] is True]
+    user_todo = [item for item in todo_data if item["userId"] == user_id]
+    comp_todo = [item for item in user_todo if item["completed"] is True]
 
-EMPLOYEE_NAME = user_data.get('name')
-TOTAL_NUMBER_OF_TASKS = len(user_todo)
-NUMBER_OF_DONE_TASKS = len(comp_todo)
+    EMPLOYEE_NAME = user_data.get('name')
+    TOTAL_NUMBER_OF_TASKS = len(user_todo)
+    NUMBER_OF_DONE_TASKS = len(comp_todo)
 
-print(f"Employee {EMPLOYEE_NAME} is done with\
- tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    print(f"Employee {EMPLOYEE_NAME} is done with\
+     tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
 
-for item in comp_todo:
-    print(f"     {item.get('title')}")
+    for item in comp_todo:
+        print(f"     {item.get('title')}")
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,37 +6,35 @@ from urllib.request import urlopen
 import sys
 
 
-todo_url = "https://jsonplaceholder.typicode.com/todos"
-with urlopen(todo_url) as todo_resp:
-    todo = todo_resp.read()
-todo_data = json.loads(todo)
+if __name__ == "__main__":
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    with urlopen(todo_url) as todo_resp:
+        todo = todo_resp.read()
+    todo_data = json.loads(todo)
 
-user_url = f"https://jsonplaceholder.typicode.com/users"
-with urlopen(user_url) as user_resp:
-    user = user_resp.read()
-user_data = json.loads(user)
-dict_t = {}
-for user in user_data:
-    user_id = user.get("id")
-    user_todo = [item for item in todo_data if item["userId"] == user_id]
-    # comp_todo = [item for item in user_todo if item["completed"] is True]
+    user_url = f"https://jsonplaceholder.typicode.com/users"
+    with urlopen(user_url) as user_resp:
+        user = user_resp.read()
+    user_data = json.loads(user)
+    dict_t = {}
+    for user in user_data:
+        user_id = user.get("id")
+        user_todo = [item for item in todo_data if item["userId"] == user_id]
 
-    username = user.get('username')
-    EMPLOYEE_NAME = user.get('name')
-    # TOTAL_NUMBER_OF_TASKS = len(user_todo)
-    # NUMBER_OF_DONE_TASKS = len(comp_todo)
+        username = user.get('username')
+        EMPLOYEE_NAME = user.get('name')
 
-    task_l = []
-    for item in user_todo:
-        task_dict = {}
-        task_dict["username"] = username
-        task_dict["task"] = item.get('title')
-        task_dict["completed"] = item.get('completed')
-        task_l.append(task_dict)
+        task_l = []
+        for item in user_todo:
+            task_dict = {}
+            task_dict["username"] = username
+            task_dict["task"] = item.get('title')
+            task_dict["completed"] = item.get('completed')
+            task_l.append(task_dict)
 
-    dict_t[str(user_id)] = task_l
+        dict_t[str(user_id)] = task_l
 
-filename = "todo_all_employees.json"
-with open(filename, 'w', encoding="utf8") as f:
-    json_str = json.dumps(dict_t, indent=4)
-    f.write(json_str)
+    filename = "todo_all_employees.json"
+    with open(filename, 'w', encoding="utf8") as f:
+        json_str = json.dumps(dict_t, indent=4)
+        f.write(json_str)
