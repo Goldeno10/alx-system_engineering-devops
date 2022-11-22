7274e5744c7aa93054cb2e0933bd3632980c4b9f
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-11-22 16:06:54 +0100

    ...

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,10 +8,10 @@ from urllib.request import urlopen
 import json
 
 if __name__ == "__main__":
-    #user id gotten from command line input
+    # user id gotten from command line input
     user_id = int(sys.argv[1])
 
-    #url to "todo" API
+    # url to "todo" API
     todo_url = "https://jsonplaceholder.typicode.com/todos"
     with urlopen(todo_url) as todo_resp:
         todo = todo_resp.read()
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     user_todo = [item for item in todo_data if item["userId"] == user_id]
     comp_todo = [item for item in user_todo if item["completed"] is True]
 
-    # Username nomber of tasks and number of comleted task as goten from the API 
+    # Username,number of tasks, number of completed tasks
     EMPLOYEE_NAME = user_data.get('name')
     TOTAL_NUMBER_OF_TASKS = len(user_todo)
     NUMBER_OF_DONE_TASKS = len(comp_todo)
