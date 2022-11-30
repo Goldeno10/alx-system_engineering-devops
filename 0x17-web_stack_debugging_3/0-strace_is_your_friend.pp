40df9d6becdc5a5d76baf65270d7dc094bbbd7cd
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-11-30 18:18:07 +0100

    Added puppet conf file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Automate the fixing of Apache 500 error.
+
+exec { 'replace_phpp_with_php':
+    provider => shell,
+    command => ["sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"; "service apache2 reload"],
+}
